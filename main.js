5bac73f8a39efb21369cfbbaff4d6682cbd01eee
samyakd546 <72605397+samyakd546@users.noreply.github.com>
2021-10-01 20:57:39 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
-function preload(){
-}
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
 
 function setup(){
 video=createCapture(VIDEO);
 video.size(550, 500);
+
 canvas=createCanvas(550, 550);
 canvas.position(560, 150);
-
 posenet = ml5.poseNet(video, modelLoaded);
 posenet.on('pose', gotposes);
 }
 
 function modelLoaded(){
-console.log("Model is loaded");
+console.log("Posenet is Initialized");
 }
 
-function gotposes(result){
-    if(result.length>0){
-        console.log(result);
+function gotposes(results) 
+{
+    if(results.length > 0)
+    console.log(results);
+
+    leftWristX = results[0].pose.leftWrist.x
+    rightWristX = results[0].pose.rightWrist.x
+    difference = floor(leftWristX - rightWristX);
+
     }
-}
+  
+    
 
 function draw(){
     background('#1ec99f');
-    
+    textSize(difference);
+    fill('#FFE787');
+    text('Samyak', 50, 400);
 }
